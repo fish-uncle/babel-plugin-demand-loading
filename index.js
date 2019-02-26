cdcf27a19f0531e54f6abb4f39ace96025edc076
fish-uncle <fishuncle@126.com>
2019-02-26 20:48:15 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const babel = require('@babel/core');
+const types = require('@babel/types');
+
+module.exports = function (api, options, dirname) {
+  return {
+    visitor: {
+      ImportDeclaration(path, ref = {}) {
+        let {opts} = ref;
+        let node = path.node;
+        let specifiers = node.specifiers;
+        if (opts.library == node.source.value && !types.isImportDeclaration(specifiers[0])) {
+          let newImport = specifiers.map((specifier) => (
+            types.importDeclaration([types.ImportDefaultSpecifier(specifier.local)], types.stringLiteral(`${node.source.value}/${specifier.local.name}`))
+          ));
+          path.replaceWithMultiple(newImport)
+        }
+      }
+    }
+  }
+};
