13db7896ed792abb49cf3106ba2ecaf1deeb9452
fish-uncle <fishuncle@126.com>
2019-02-26 20:53:01 +0800

    init

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const babel = require('@babel/core');
-const types = require('@babel/types');
+const babel = require('babel-core');
+const types = require('babel-types');
 
-module.exports = function (api, options, dirname) {
-  return {
-    visitor: {
-      ImportDeclaration(path, ref = {}) {
-        let {opts} = ref;
-        let node = path.node;
-        let specifiers = node.specifiers;
-        if (opts.library == node.source.value && !types.isImportDeclaration(specifiers[0])) {
-          let newImport = specifiers.map((specifier) => (
-            types.importDeclaration([types.ImportDefaultSpecifier(specifier.local)], types.stringLiteral(`${node.source.value}/${specifier.local.name}`))
-          ));
-          path.replaceWithMultiple(newImport)
-        }
+module.exports = {
+  visitor: {
+    ImportDeclaration(path, ref = {}) {
+      let {opts} = ref;
+      let node = path.node;
+      let specifiers = node.specifiers;
+      if (opts.library == node.source.value && !types.isImportDeclaration(specifiers[0])) {
+        let newImport = specifiers.map((specifier) => (
+          types.importDeclaration([types.ImportDefaultSpecifier(specifier.local)], types.stringLiteral(`${node.source.value}/${specifier.local.name}`))
+        ));
+        path.replaceWithMultiple(newImport)
       }
     }
   }
